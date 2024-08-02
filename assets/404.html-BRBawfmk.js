8e0932735b7c76454abda50e564c584076bdc2c5
PlayDrinkEatCode <PlayDrinkEatCode@users.noreply.github.com>
2024-08-02 12:34:12 +0000

    deploy: 49855183fd558b987d640801c363fc250e016cb9

---
+++ assets/404.html-BRBawfmk.js
@@ -0,0 +1,1 @@
+import{_ as t,c as e,o,a as n}from"./app-BOWkcTM0.js";const a={},c=n("p",null,"404 Not Found",-1),s=[c];function _(r,l){return o(),e("div",null,s)}const m=t(a,[["render",_],["__file","404.html.vue"]]),h=JSON.parse('{"path":"/404.html","title":"","lang":"en-US","frontmatter":{"layout":"NotFound"},"headers":[]}');export{m as comp,h as data};
